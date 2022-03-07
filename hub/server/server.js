44474faae7b2667562a8fc717fe055c69cdc8d40
Dan <dannapert@gmail.com>
2022-03-07 09:44:54 -0500

    hopfully fixed server

--- hub/server/server.js
+++ hub/server/server.js
@@ -1,5 +1,6 @@
-const mqtt = require('mqtt')
-const client  = mqtt.connect('tcp://localhost:1883')
+const mqtt = require('tcp')
+const client  = mqtt.connect('tcp://127.0.0.1:1883')
+
 
 client.on('connect', function () {
     client.subscribe('presence', function (err) {
