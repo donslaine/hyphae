15dc327bcdd90901b261e8ba1e8e3c99d2ee7965
Dnapert <98182613+Dnapert@users.noreply.github.com>
2022-03-07 11:33:55 -0500

    Update server.js

--- hub/server/server.js
+++ hub/server/server.js
@@ -1,5 +1,5 @@
-const mqtt = require('tcp')
-const client  = mqtt.connect('tcp://127.0.0.1:1883')
+const mqtt = require('mqtt')
+const client  = mqtt.connect('mqtt://192.168.117.1:1883')
 
 
 client.on('connect', function () {
